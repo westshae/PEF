800ea4b8fa662856811d40802a7646518e0dea11
westshae <shaewest02@gmail.com>
2021-12-25 09:27:08 +1300

    Iterate through settings to display

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,102 +1,104 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 //Component imports
-import Navigation from "../components/Navigation"
-import {Container, Content} from '../components/Containers'
-import Footer from '../components/Footer'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import styled from 'styled-components'
+import Navigation from "../components/Navigation";
+import { Container, Content } from "../components/Containers";
+import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
 
-interface SettingsInterface{
-  city?:string,
-  country?:string,
-  balance?:number,
-  profession?:string,
+interface SettingsInterface {
+  city?: string;
+  country?: string;
+  balance?: number;
+  profession?: string;
 }
 
-const Form = styled.form`
+const Form = styled.form``;
 
-`
+const Label = styled.label``;
 
-const Label = styled.label`
+const Button = styled.button``;
 
-`
+const Checkbox = styled.input``;
 
-const Button = styled.button`
+const InputField = styled.input``;
 
-`
-
-const Checkbox = styled.input`
-
-`
-
-const InputField = styled.input`
-
-`
-
-const toggleSetting = (label:string, currentValue:boolean) =>{
-  return(
-      <div>
-
-<Label>{label}</Label>
-        <Checkbox type="checkbox" defaultChecked={currentValue}/>
-      </div>
-  )
-}
-
-const inputSetting = (label:string, currentValue:string) =>{
-  return(
+const toggleSetting = (label: string, currentValue: boolean) => {
+  return (
     <div>
-
-<Label>{label}</Label>
-        <InputField/>
+      <Label>{label}</Label>
+      <Checkbox type="checkbox" defaultChecked={currentValue} />
     </div>
+  );
+};
+
+const inputSetting = (label: string, currentValue: string) => {
+  return (
+    <div>
+      <Label>{label}</Label>
+      <InputField placeholder={currentValue}/>
+    </div>
+  );
+};
+
+const FormContainer = (settings:SettingsInterface) =>{
+  let values = [];
+  console.log(settings);
+  for(const [key, value] of Object.entries(settings)){
+    if(typeof value === "boolean"){
+      values.push(toggleSetting(key, value));
+    }else{
+      values.push(inputSetting(key, value));
+    }
+  }
+  return(
+    <Form>
+      {values}
+      <Button type="submit">Submit</Button>
+
+    </Form>
   )
 }
 
 const Settings: NextPage = () => {
-  let email:string;
-  let token:string;
-  try{
+  let email: string;
+  let token: string;
+  try {
     email = window.localStorage.getItem("email")!;
     token = window.localStorage.getItem("token")!;
-  }catch(e){
+  } catch (e) {
     console.error(e);
   }
 
   const [settings, setSettings] = useState<SettingsInterface>({});
 
-  useEffect(()=>{
-    if(email !== undefined && token !== undefined){
-      axios.get("http://localhost:5000/auth/settings", {
-      params:{
-        email:email,
-        token:token
-      }
-    }).then((res)=>{
-      setSettings(res.data);
-    })
+  useEffect(() => {
+    if (email !== undefined && token !== undefined) {
+      axios
+        .get("http://localhost:5000/auth/settings", {
+          params: {
+            email: email,
+            token: token,
+          },
+        })
+        .then((res) => {
+          setSettings(res.data);
+        });
     }
-  }, [])
+  }, []);
 
   return (
     <Container>
-      <Navigation/>
+      <Navigation />
       <Content>
         <h1>Settings</h1>
-        <h1>{settings.profession}</h1>
-        <Form>
-
-        {toggleSetting("ree", true)}
-        {inputSetting("woo", "default")}
-          <Button type="submit">Submit</Button>
-        </Form>
-
+        {FormContainer(settings)}
       </Content>
-      <Footer/>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
 export default Settings;
