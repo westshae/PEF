007e0927db88a17f35fe83cbd7418e5069ded7e2
westshae <shaewest02@gmail.com>
2022-01-02 19:24:40 +1300

    Image via link from backend basics

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,16 +2,41 @@ import type { NextPage } from 'next'
 
 //Component imports
 import Navigation from "../components/Navigation"
-import UserInput from '../components/UserInput'
 import {Container, Content} from '../components/Containers'
 import Footer from '../components/Footer'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Profile: NextPage = () => {
+  let email: string;
+  let token: string;
+  try {
+    email = window.localStorage.getItem("email")!;
+    token = window.localStorage.getItem("token")!;
+  } catch (e) {
+    console.error(e);
+  }
+
+  const [backgroundLink, setBackgroundLink] = useState("");
+
+  useEffect(()=>{
+    axios.get("http://localhost:5000/profile/pfp", {
+      params:{
+        email:email,
+        token:token,
+        photoname:"example1"
+      }
+    })
+    .then((res)=>{
+      setBackgroundLink(res.data);
+    })
+  }, [])
   return (
     <Container>
       <Navigation/>
       <Content>
         <h1>Profile</h1>
+        <img src={backgroundLink}/>
       </Content>
       <Footer/>
     </Container>
