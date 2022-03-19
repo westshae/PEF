aa6832eb4055ca811e59d534f67e3f10f219e0f0
westshae <shaewest02@gmail.com>
2022-03-19 20:02:23 +1300

    submit button styles

--- src/components/feed/newPost.tsx
+++ src/components/feed/newPost.tsx
@@ -1,20 +1,33 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-
+  display:flex;
 `;
 
 const TextBox = styled.textarea`
   width:100%;
-  background-color:#292929;
   border:none;
   margin:1rem;
+  margin-right:0.5rem;
+  background-color: #292929;
+  color: #ebebeb;
+`;
+
+const SubmitButton = styled.button`
+  margin:1rem;
+  margin-left:0.5rem;
+  background-color: #292929;
+  color: #ebebeb;
+  border:none;
+  padding-left:0.8rem;
+  padding-right:0.8rem;
 `;
 
 const newPost = () => {
   return (
     <Container>
       <TextBox/>
+      <SubmitButton>Post</SubmitButton>
     </Container>
   );
 };
